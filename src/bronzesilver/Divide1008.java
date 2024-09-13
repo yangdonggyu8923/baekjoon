6191f4cfc21c64b833ffa306012c5d449ba69b60
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-09-13 17:34:57 +0900

    New Repo

---
+++ src/bronzesilver/Divide1008.java
@@ -0,0 +1,12 @@
+package bronzesilver;
+
+import java.util.Scanner;
+
+public class Divide1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        System.out.println(a/b);
+    }
+}
