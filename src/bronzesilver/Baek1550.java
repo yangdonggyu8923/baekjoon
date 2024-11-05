f0543112cb93c3608e49f8fe6b3b9f696257599d
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-11-05 22:03:23 +0900

    baek 1550:

---
+++ src/bronzesilver/Baek1550.java
@@ -0,0 +1,12 @@
+package bronzesilver;
+
+import java.util.Scanner;
+
+public class Baek1550 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(Integer.parseInt(input,16));
+        sc.close();
+    }
+}
