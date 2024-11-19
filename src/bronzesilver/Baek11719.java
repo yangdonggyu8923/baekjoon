3093579780305be02ece40a8b0b1dc60b4c77674
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-11-19 23:11:30 +0900

    baek 11719

---
+++ src/bronzesilver/Baek11719.java
@@ -0,0 +1,13 @@
+package bronzesilver;
+
+import java.io.*;
+
+public class Baek11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
