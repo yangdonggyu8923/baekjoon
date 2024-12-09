0d58427eb1d1d00a044d9d952183b6359001ad03
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-12-09 22:45:42 +0900

    baek 1964

---
+++ src/bronzesilver/Baek1964.java
@@ -0,0 +1,22 @@
+package bronzesilver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek1964 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long dot = 7;
+        long sum = 5;
+
+        for(int i = 1; i < N; i++) {
+            sum += dot;
+            dot += 3;
+        }
+        System.out.println(sum % 45678);
+    }
+
+}
